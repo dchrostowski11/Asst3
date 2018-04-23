903feeb6343ee842d1493c66a53d25d253c64a6d
dchrostowski11 <danielchrostowski11@gmail.com>
2018-04-23 19:11:58 -0400

    Update libnetfiles.c

--- libnetfiles.c
+++ libnetfiles.c
@@ -9,6 +9,30 @@ int netopen(const char *pathname, int flags){
 	// must include one of the following access modes: O_RDONLY, O_WRONLY, O_RDWR
 	// returns new file descriptor, or -1 on error
 	// fd should be negative numbers but not negative 1
+	int fd = -2;
+	
+	// check to make sure the mode is correct
+	const char *access_mode;
+	if(flags == O_RDONLY){
+		access_mode = "O_RDONLY";
+	}
+	else if(flags == O_WRONLY){
+		access_mode = "O_WRONLY";
+	}
+	else if(flags == O_RDWR){
+		access_mode = "O_RDWR";
+	} else {
+		perror("invalid access mode!");
+		return -1;
+	}
+	
+	//make a socket
+	int socket = socket(AF_INET, SOCK_STREAM, 0);
+	if(socket < 0){
+		perror("couldn't create socket");
+	}
+	
+	
 }
 
 ssize_t netread(int fildes, void *buf, size_t nbyte){
